14854063b41f42993f354b74d03a89738ae4acc5
Amit Dayal <adayal@vt.edu>
2018-04-11 16:05:19 -0400

    adding ability to log all requests, responses, errors. Also created logs API allows users to extract specific logs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var mongourl = "mongodb://localhost:27017";
 var dbName = "GraphQuery"
 var mongoDB = null;
-
+var collectionName = "logs"
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 3001,
@@ -19,6 +19,9 @@ bb.extend(app, {
 var routes = require('./lib/routes/routes.js');
 routes(app);
 app.listen(port);
+exports.getCollectionName = function() {
+	return collectionName 
+}
 exports.getDB = function(callback) {
 	if (!mongoDB) {
 		MongoClient.connect(mongourl, function(err, client) {
@@ -27,8 +30,13 @@ exports.getDB = function(callback) {
 			} else {
 				console.log("connected successfully")
 				mongoDB = client.db(dbName)
+				mongoDB.createCollection(collectionName, function(err, res) {
+					if (err) throw err
+					console.log("collection created")
+					callback(mongoDB)
+				})
 				//client.close()
-				callback(mongoDB)
+				//callback(mongoDB)
 			}
 		});
 	} else {
